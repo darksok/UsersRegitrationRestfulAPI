cfb68fccfae97bec087d6517485f0ba64ca5d9f1
Alonso de la Barra <alonso.delabarra@gmail.com>
2020-08-03 04:38:41 -0400

    base code

--- src/test/java/com/users/security/SecurityConfigurationTest.java
+++ src/test/java/com/users/security/SecurityConfigurationTest.java
@@ -1,22 +1,21 @@
-package com.users;
+package com.users.security;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.users.api.UserApi;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
-class UsersApplicationTests {
+class SecurityConfigurationTest {
 
 	@Autowired
-	private UserApi userApi;
+	private BCryptPasswordEncoder encoder;
 	
 	@Test
-	void contextLoads() {
-		assertThat(userApi).isNotNull();
+	void passwordEncoderExists() {
+		assertThat(encoder).isNotNull();
 	}
 
 }
